d99b9964ab8fe7ed28bdfb201ead28a85d3a3763
viramjain13@gmail.com <viramjain13@gmail.com>
2021-02-05 20:21:45 +0530

    template fix

---
+++ dash/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(req):
+    return render(req,'index.html')
+def about(req):
+    return render(req,'about.html')
+def contact(req):
+    return render(req,'contacts.html')
