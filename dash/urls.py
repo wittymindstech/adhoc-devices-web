a31e9440e70bf70f62d30de1e994b5f2a06a0c61
viramjain13@gmail.com <viramjain13@gmail.com>
2021-02-06 21:14:41 +0530

    product and gallery page added

--- dash/urls.py
+++ dash/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
+    path('product/',views.product,name='product'),
+    path('gallery/',views.gallery,name='gallery'),
 ]
