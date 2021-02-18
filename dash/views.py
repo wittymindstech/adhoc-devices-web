338e207a1a9ca734506b3a3c4ca8e4a55484b527
viramjain <42388215+viramjain@users.noreply.github.com>
2021-02-18 18:03:44 +0530

    dynamic product and home page

--- dash/views.py
+++ dash/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import  Product
 # Create your views here.
 def home(req):
-    return render(req,'index.html')
+    items=Product.objects.all()
+    d={'items':items}
+    return render(req,'index.html',d)
 def about(req):
     return render(req,'about.html')
 def contact(req):
     return render(req,'contacts.html')
+
 def product(req):
-    return render(req,'shop.html')
+    items=Product.objects.all()
+    l=list(items)
+    date_wise_sorted_list=sorted(l,key=lambda x:x.date,reverse=True)
+    print(date_wise_sorted_list)
+    d={'items':items,'new_product':date_wise_sorted_list}
+    return render(req,'shop.html',d)
 def gallery(req):
     return render(req,'gallery.html')
