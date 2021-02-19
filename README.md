3a15dfecaa4b3062e7dd3e2963d9e35994a3d12d
AmarAgarwal <60135473+AmarAgarwal@users.noreply.github.com>
2021-02-19 21:45:39 +0530

    final working

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # adhoc-devices-web
 Website for IoT and AR VR Devices
-# steps to install django project 
-`pip install -r requirements.txt`
-
-`python manage.py runserver`
- 
--- iotdash/settings.py
+++ iotdash/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import  os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'iotdash.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
 
+LOGOUT_REDIRECT_URL = '/'
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -118,4 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
